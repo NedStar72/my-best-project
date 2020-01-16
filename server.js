53eaaec4630f57b345c154de76bf128cdb66a7b3
NedStar <torgashin99@gmail.com>
2020-01-16 22:29:46 +0500

    Add Next.js

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const next = require('next')
 
+// Руты
+const anime = require('./routes/anime');
+
+// Подключение к БД
 mongoose.connect('mongodb://localhost/shiki', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Сonnection succesful!'))
     .catch((err) => console.error(err));
 
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const app = next(process.env.NODE_ENV !== 'production')
+app.prepare().then(() => {
+    const server = express();
 
-const anime = require('./routes/anime');
+    server.use(logger('dev'));
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({ 'extended': 'false' }));
+    //server.use(express.static(path.join(__dirname, 'build')));
 
-const server = express();
+    // Api
+    server.use('/api/anime', anime);
 
-server.use(logger('dev'));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ 'extended': 'false' }));
-//server.use(express.static(path.join(__dirname, 'build')));
+    // Next.js
+    server.get('/a', (req, res) => {
+        return app.render(req, res, '/example', req.query)
+      })
 
-server.use('/api/anime', anime);
+    const port = process.env.PORT || '3000';
 
-const port = process.env.PORT || '3000';
-
-server.listen(port, () => console.log(`http://localhost:${port}/`));
+    server.listen(port, () => console.log(`http://localhost:${port}/`));
+})
